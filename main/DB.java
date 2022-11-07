4cb0e1d75ea1bbc8c73dadaa4d3522fdc5820ec6
aexaekaso <46701146+aexaekaso@users.noreply.github.com>
2022-11-07 19:01:56 +0900

    22.11.07 유채원 Home, DB, Customer, Account 클래스 수정

--- main/DB.java
+++ main/DB.java
@@ -7,28 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DB {
-	Connection CN = null; // 데이터 베이스와 연결을 위한 객체
-	PreparedStatement PS = null; // sql 문을 데이터베이스에 보내기 위한 객체, ?로 처리
-	Statement stmt = null; // sql 문을 데이터베이스에 보내기 위한 객체, 전체를 받을 때
-	ResultSet RS = null; // sql 질의에 의해 생성된 테이블을 저장하는 객체
-	BufferedReader br = null; // 입력받기 위한 객체
+	Connection CN = null;
+	PreparedStatement PS = null;
+	ResultSet RS = null;
 	
-	String url = "jdbc:mysql://localhost:3306/cafedb?characterEncoding=UTF-8&serverTimezone=UTC";
-	String user = "root"; // 사용자
-	String password = "1234"; // 암호
-	String driver = "com.mysql.cj.jdbc.Driver";
-	String[] mysqlCode; //메뉴에 따른 SQL문 저장리스트
-	
-	// 기본생성자
-	public DB() {
-
-	}
 	
 	//데이터베이스 연결
 	public void connectDB() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://cafedb?characterEndofing=UTF-8&serverTimezone=Asia/Seoul";
+			String url = "jdbc:mysql://localhost:3306/cafeorder?characterEndofing=UTF-8&serverTimezone=UTC";
 			CN = DriverManager.getConnection(url, "test", "1234");
 			System.out.println("*** 데이터베이스 연결 성공 ***");  //나중에 제거
 			
